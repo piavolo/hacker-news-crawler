0feded83889dbc81bd6109fac92681fc6aa236ef
Pablo Gandarillas <XV.I.MCMLXXXIX@gmail.com>
2024-07-18 22:49:19 +0200

    Filters integration in Entries controller and add filter buttons to main view

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -6,7 +6,7 @@ class EntriesController < ApplicationController
     url = 'https://news.ycombinator.com/'
     document = Nokogiri::HTML(URI.open(url))
 
-    @entries = []
+    entries_data = []
 
     document.css('.athing').first(30).each do |athing|
       number = athing.css('.rank').text.to_i
@@ -15,9 +15,23 @@ class EntriesController < ApplicationController
       points = subtext.css('.score').text.to_i
       comments = subtext.css('a').last.text.split.first.to_i
 
-      entry = Entry.find_or_initialize_by(number: number)
-      entry.update(title: title, points: points, comments: comments)
-      @entries << entry
+      entries_data << { number: number, title: title, points: points, comments: comments }
+    end
+
+    entries_data.first(30).each do |entry_data|
+      entry = Entry.find_or_initialize_by(number: entry_data[:number])
+      entry.update(title: entry_data[:title], points: entry_data[:points], comments: entry_data[:comments])
+    end
+
+    @entries = Entry.limit(30)
+
+    if params[:filter].present?
+      case params[:filter]
+      when 'more_than_five_words'
+        @entries = @entries.more_than_five_words.ordered_by_comments
+      when 'five_or_less_words'
+        @entries = @entries.five_or_less_words.ordered_by_points
+      end
     end
   end
 end
