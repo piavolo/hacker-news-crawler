2b43eceb3b5e7b80539fef345835f2978fa6aefa
Pablo Gandarillas <XV.I.MCMLXXXIX@gmail.com>
2024-07-18 19:36:12 +0200

    Set up routes, main view and Entries controller using Nokogiri to show the top 30 entries

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -1,4 +1,23 @@
+require 'nokogiri'
+require 'open-uri'
+
 class EntriesController < ApplicationController
   def index
+    url = 'https://news.ycombinator.com/'
+    document = Nokogiri::HTML(URI.open(url))
+
+    @entries = []
+
+    document.css('.athing').first(30).each do |athing|
+      number = athing.css('.rank').text.to_i
+      title = athing.css('.titleline a').text
+      subtext = athing.next_element.css('.subtext')
+      points = subtext.css('.score').text.to_i
+      comments = subtext.css('a').last.text.split.first.to_i
+
+      entry = Entry.find_or_initialize_by(number: number)
+      entry.update(title: title, points: points, comments: comments)
+      @entries << entry
+    end
   end
 end
