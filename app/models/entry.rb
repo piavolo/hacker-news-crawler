94ad670c3e8f3481a5b9fb2a91377c301b0873ce
Pablo Gandarillas <XV.I.MCMLXXXIX@gmail.com>
2024-07-18 22:42:29 +0200

    Add filters to Entry model

--- app/models/entry.rb
+++ app/models/entry.rb
@@ -1,2 +1,6 @@
 class Entry < ApplicationRecord
+  scope :more_than_five_words, -> { where("LENGTH(title) - LENGTH(REPLACE(title, ' ', '')) + 1 > 5") }
+  scope :five_or_less_words, -> { where("LENGTH(title) - LENGTH(REPLACE(title, ' ', '')) + 1 <= 5") }
+  scope :ordered_by_comments, -> { order(comments: :desc) }
+  scope :ordered_by_points, -> { order(points: :desc) }
 end
